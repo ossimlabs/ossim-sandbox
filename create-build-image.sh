a6c774c81228c4344d5208642e1a75c002061933
Garrett Potts <gpotts@radiantblue.com>
2019-09-09 09:24:39 -0400

    Adding artifact

--- create-build-image.sh
+++ create-build-image.sh
@@ -7,4 +7,5 @@ source $CREATE_BUILD_IMAGE_SCRIPT_DIR/common.sh $*
 
 docker build -t ossim-build-$TYPE -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
+docker save ossim-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/save ossim-build-$TYPE.tgz
 exit 0
