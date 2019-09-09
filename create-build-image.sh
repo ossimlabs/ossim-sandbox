3f44656d581bbeac9e46622df46e1781b9cacba7
Garrett Potts <gpotts@radiantblue.com>
2019-09-09 09:40:03 -0400

    Added TYPE to the docker creation and artifact

--- create-build-image.sh
+++ create-build-image.sh
@@ -5,7 +5,7 @@ popd >/dev/null
 
 source $CREATE_BUILD_IMAGE_SCRIPT_DIR/common.sh $*
 
-docker build -t ossim-build-$OS_ID-$OS_VERSION -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker
+docker build -t ossim-build-$TYPE -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
-docker save ossim-build-$OS_ID-$OS_VERSION | gzip -c > $OSSIM_DEV_HOME/save ossim-build--$OS_ID-$OS_VERSION.tgz
+docker save ossim-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/save ossim-build-$TYPE.tgz
 exit 0
