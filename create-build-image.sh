4e33bbb609de54c67fa4dfa12f8384c7c6de60d3
Garrett Potts <gpotts@radiantblue.com>
2019-09-09 09:43:31 -0400

    Fixed the extra characters in the save

--- create-build-image.sh
+++ create-build-image.sh
@@ -9,5 +9,5 @@ echo docker build -t ossim-build-$TYPE -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/d
 docker build -t ossim-build-$TYPE -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
 echo docker save ossim-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/save ossim-build-$TYPE.tgz
-docker save ossim-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/save ossim-build-$TYPE.tgz
+docker save ossim-build-$TYPE | gzip -c > ossim-build-$TYPE.tgz
 exit 0
