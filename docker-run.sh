4baed17de44700399a7892568c27aa125041f888
Garrett Potts <gpotts@radiantblue.com>
2019-09-10 08:13:48 -0400

    Added arguments

--- docker-run.sh
+++ docker-run.sh
@@ -6,13 +6,50 @@ fi
 pushd `dirname ${BASH_SOURCE[0]}` >/dev/null
 DOCKER_RUN_SCRIPT=`pwd -P`
 popd > /dev/null
+ARGS_TO_PASS=""
+INTERACTIVE=false
+DATA="/data"
+WORKING_DIR="/home/ossim/ossimlabs"
+while [[ $# -gt 0 ]] ;
+do
+    opt="$1";
+    shift;              #expose next argument
+    echo $opt
+    case "$opt" in
+        "--interactive" )
+           INTERACTIVE="$1"; 
+           shift
+           ;;
+           "--data" )
+           DATA=$1;
+           shift
+           ;;
+           "--root-dir" )
+           ROOT_DIR=$1;
+           shift
+           ;;
+           "--working-dir" )
+           WORKING_DIR=$1;
+           shift
+           ;;
+        *)
+        ARGS_TO_PASS="$ARGS_TO_PASS $opt"; 
+        ;;
+   esac
+done
+
 source $DOCKER_RUN_SCRIPT/common.sh
 if [ "${ROOT_DIR}" == "" ] ; then 
    pushd $DOCKER_RUN_SCRIPT/.. > /dev/null
    ROOT_DIR=`pwd -P`
    popd > /dev/null
 fi
-docker run -u "$(id -u ${USER}):$(id -g ${USER})" --net=host --ipc host --env="DISPLAY" --rm -w /home/ossim/ossimlabs --mount type=bind,source=/data,target=/data --mount type=bind,source=$ROOT_DIR,target=/home/ossim/ossimlabs $*
-if [ $? -ne 0 ]; then echo "ERROR: Failed execution of $*" ; exit 1 ; fi
+
+if $INTERACTIVE ; then
+docker run -it -u "$(id -u ${USER}):$(id -g ${USER})" --net=host --ipc host --env="DISPLAY" --rm -w $WORKING_DIR --mount type=bind,source=$DATA,target=/data --mount type=bind,source=$ROOT_DIR,target=/home/ossim/ossimlabs $ARGS_TO_PASS
+else
+docker run -u "$(id -u ${USER}):$(id -g ${USER})" --net=host --ipc host --env="DISPLAY" --rm -w $WORKING_DIR --mount type=bind,source=$DATA,target=/data --mount type=bind,source=$ROOT_DIR,target=/home/ossim/ossimlabs $ARGS_TO_PASS
+fi
+if [ $? -ne 0 ]; then echo "ERROR: Failed execution of $ARGS_TO_PASS" ; exit 1 ; fi
 exit 0
 
