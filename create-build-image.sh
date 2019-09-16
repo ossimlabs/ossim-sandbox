1417efd88d08a28c7c57a8a42cba53e4976a33b6
Garrett Potts <gpotts@radiantblue.com>
2019-09-16 15:18:06 -0400

    Adding output

--- create-build-image.sh
+++ create-build-image.sh
@@ -4,7 +4,7 @@ CREATE_BUILD_IMAGE_SCRIPT_DIR=`pwd -P`
 popd >/dev/null
 
 source $CREATE_BUILD_IMAGE_SCRIPT_DIR/common.sh $*
-
+echo "BRANCH_NAME = ${BRANCH_NAME}"
 docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-build-$TYPE -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
 docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-build-$TYPE-jenkins -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins
