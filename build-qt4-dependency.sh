5d39fe33a9106cf428633f4ca73d71765e40d4a7
oscarkramer <oscar.kramer@radiantsolutions.com>
2019-09-27 17:00:42 -0400

    Debugging for QT4

--- build-qt4-dependency.sh
+++ build-qt4-dependency.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-pushd `dirname ${BASH_SOURCE[0]}`/..
+
+pushd `dirname ${BASH_SOURCE[0]}` >/dev/null
+BUILD_OSSIM_SCRIPT_DIR=`pwd -P`
+popd
+pushd `dirname ${BASH_SOURCE[0]}` >/dev/null
+
 export OSSIM_DEV_HOME=`pwd -P`
+echo "SOURCING: $BUILD_OSSIM_SCRIPT_DIR/common.sh $1"
+source $BUILD_OSSIM_SCRIPT_DIR/common.sh $1
 
 export QT4=qt-4.8.7
 echo "OSSIM_MAKE_JOBS  = $OSSIM_MAKE_JOBS"
@@ -16,14 +23,19 @@ fi
 
 if [ -d $OSSIM_DEV_HOME/$QT4 ] ; then
    pushd $OSSIM_DEV_HOME/$QT4
-   ./configure
+   ./configure -opensource -confirm-license
+   if [ $? -ne 0 ]; then
+      echo "QT4 configuration error: $error"
+      exit 1
+   fi
    make -j $OSSIM_MAKE_JOBS
    if [ $? -ne 0 ]; then
       echo "QT4 build error: $error"
       exit 1
    fi
 else
-   echo "Error: $OSSIM_DEV_HOME/$QT4 Not found.  Please edit build-qt4-dependency.sh to specify the proper version then place the version under https://s3.amazonaws.com/ossimlabs/dependencies/source/"
+   echo "Error: $OSSIM_DEV_HOME/$QT4 Not found.  Please edit build-qt4-dependency.sh to specify the proper "
+   echo "version then place the version under https://s3.amazonaws.com/ossimlabs/dependencies/source/"
    exit 1  
 fi
 
