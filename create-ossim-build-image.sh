bc19c3a321af0c467f9a845a63a8447a3dd4ba34
Garrett Potts <gpotts@radiantblue.com>
2019-11-14 09:36:02 -0500

    Added build image for ossim

--- create-ossim-build-image.sh
+++ create-ossim-build-image.sh
@@ -5,10 +5,9 @@ popd >/dev/null
 
 source $CREATE_BUILD_IMAGE_SCRIPT_DIR/common.sh $*
 
-docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-base-build-$TYPE:$DOCKER_LABEL -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/base-build/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/base-build
+docker build -t ossim-build-$TYPE:$DOCKER_LABEL -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/ossim-build/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
 # docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-build-$TYPE-jenkins:$DOCKER_LABEL -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins
 # if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
-docker save ossim-base-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/ossim-base-build-$TYPE.tgz
-#docker save ossim-build-$TYPE-jenkins | gzip -c > $OSSIM_DEV_HOME/ossim-build-$TYPE-jenkins.tgz
+#docker save ossim-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/ossim-build-$TYPE.tgz
 exit 0
