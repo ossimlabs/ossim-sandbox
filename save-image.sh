0e7d1f3e2be0546ed31743b097f6f2f7a3eb8b9b
Garrett Potts <gpotts@radiantblue.com>
2019-11-14 13:14:31 -0500

    Added builds and artifacts

--- save-image.sh
+++ save-image.sh
@@ -4,7 +4,8 @@ SAVE_IMAGE_SCRIPT_DIR=`pwd -P`
 popd >/dev/null
 IMAGE=$1
 TAG=$2
+OUTPUT=$3
 
-docker save $IMAGE:$TAG | gzip -c >$IMAGE-$TAG.tgz
+docker save $IMAGE:$TAG | gzip -c >$OUTPUT
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
 exit 0
