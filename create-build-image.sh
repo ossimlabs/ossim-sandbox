598788a3ccfdb3dc44d45fa637a8137185e5f321
Garrett Potts <gpotts@radiantblue.com>
2019-11-14 09:21:55 -0500

    Changed to ossim-base-build

--- create-build-image.sh
+++ create-build-image.sh
@@ -5,7 +5,7 @@ popd >/dev/null
 
 source $CREATE_BUILD_IMAGE_SCRIPT_DIR/common.sh $*
 
-docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-build-$TYPE:$DOCKER_LABEL -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build
+docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-base-build-$TYPE:$DOCKER_LABEL -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
 # docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-build-$TYPE-jenkins:$DOCKER_LABEL -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins
 # if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
