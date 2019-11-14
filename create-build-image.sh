b4f635642642520fe0cef491b2dd4f748a875606
Garrett Potts <gpotts@radiantblue.com>
2019-11-14 09:20:52 -0500

    Changed name to ossim-base-build

--- create-build-image.sh
+++ create-build-image.sh
@@ -9,6 +9,6 @@ docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VER
 if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
 # docker build --build-arg GRADLE_VERSION=${GRADLE_VERSION} --build-arg GROOVY_VERSION=${GROOVY_VERSION} -t ossim-build-$TYPE-jenkins:$DOCKER_LABEL -f $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins/Dockerfile $CREATE_BUILD_IMAGE_SCRIPT_DIR/$TYPE/docker/docker-build-jenkins
 # if [ $? -ne 0 ]; then echo "ERROR: Unable to create build image" ; exit 1 ; fi
-docker save ossim-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/ossim-build-$TYPE.tgz
+docker save ossim-base-build-$TYPE | gzip -c > $OSSIM_DEV_HOME/ossim-base-build-$TYPE.tgz
 #docker save ossim-build-$TYPE-jenkins | gzip -c > $OSSIM_DEV_HOME/ossim-build-$TYPE-jenkins.tgz
 exit 0
